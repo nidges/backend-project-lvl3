4aa19f6aee9bf60dfe6659afffc32bc75d1b600f
nidges <nidges@yandex.ru>
2021-12-20 09:34:00 +0300

    linter fixes

--- src/classes/CoreHTML.js
+++ src/classes/CoreHTML.js
@@ -5,14 +5,22 @@ import fsp from 'fs/promises';
 import Source from './Source.js';
 import { getExtension, getFileName, normalizeLink } from '../utils.js';
 
-export default class CoreHTML extends Source {
-  static mapping = {
-    img: 'src',
-    link: 'href',
-    script: 'src',
-  };
+const mapping = {
+  img: 'src',
+  link: 'href',
+  script: 'src',
+};
 
-  static tags = ['img', 'link', 'script'];
+const tags = ['img', 'link', 'script'];
+
+export default class CoreHTML extends Source {
+  // static mapping = {
+  //   img: 'src',
+  //   link: 'href',
+  //   script: 'src',
+  // };
+  //
+  // static tags = ['img', 'link', 'script'];
 
   getAxiosConfig() {
     return {
@@ -26,9 +34,11 @@ export default class CoreHTML extends Source {
     const $ = cheerio.load(html);
     const { origin } = this.url;
 
-    CoreHTML.tags.forEach((tag) => {
+    // CoreHTML.tags.forEach((tag) => {
+    tags.forEach((tag) => {
       $(tag).each(function () {
-        links.push($(this).attr(CoreHTML.mapping[tag]));
+        // links.push($(this).attr(CoreHTML.mapping[tag]));
+        links.push($(this).attr(mapping[tag]));
       });
     });
 
@@ -47,9 +57,11 @@ export default class CoreHTML extends Source {
 
     // rewriting all links to core page's sources in HTML attributes
     // only links to the same third level domain pages are rewritten
-    CoreHTML.tags.forEach((tag) => {
+    // CoreHTML.tags.forEach((tag) => {
+    tags.forEach((tag) => {
       $(tag).each(function () {
-        const relativeLink = $(this).attr(CoreHTML.mapping[tag]);
+        // const relativeLink = $(this).attr(CoreHTML.mapping[tag]);
+        const relativeLink = $(this).attr(mapping[tag]);
         if (relativeLink) {
           const absoluteLink = normalizeLink(coreUrl, relativeLink);
           let resultingLink = '';
@@ -60,7 +72,8 @@ export default class CoreHTML extends Source {
             resultingLink = absoluteLink.toString();
           }
 
-          $(this).attr(CoreHTML.mapping[tag], resultingLink);
+          // $(this).attr(CoreHTML.mapping[tag], resultingLink);
+          $(this).attr(mapping[tag], resultingLink);
         }
       });
     });
